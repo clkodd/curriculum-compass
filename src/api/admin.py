7245158ef44d6d8b24bfc7660eede49c50808346
Hayley Chang <84148805+haysho2260@users.noreply.github.com>
2023-11-05 14:03:32 -0800

    Update admin.py
    add get org info and reset tables

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,28 +1,42 @@
-# from fastapi import APIRouter, Depends, Request
-# from pydantic import BaseModel
-# from src.api import auth
+from fastapi import APIRouter, Depends, Request
+from pydantic import BaseModel
+from src.api import auth
 
-# router = APIRouter(
-#     prefix="/admin",
-#     tags=["admin"],
-#     dependencies=[Depends(auth.get_api_key)],
-# )
+router = APIRouter(
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(auth.get_api_key)],
+)
 
-# @router.post("/reset")
-# def reset():
-#     """
-#     Reset the game state. Gold goes to 100, all potions are removed from
-#     inventory, and all barrels are removed from inventory. Carts are all reset.
-#     """
-#     return "OK"
+@router.post("/reset")
+def reset():
+    """
+    Reset the game state. Gold goes to 100, all potions are removed from
+    inventory, and all barrels are removed from inventory. Carts are all reset.
+    """
+    with db.engine.begin() as connection:
+        # Drop existing tables
+        connection.execute(sqlalchemy.text(
+            "TRUNCATE events, organizations, supervisors, volunteer_schedule, volunteers CASCADE"))
+        
+    return "OK"
 
 
-# @router.get("/shop_info/")
-# def get_shop_info():
-#     """ """
-
-#     # TODO: Change me!
-#     return {
-#         "shop_name": "Potion Shop",
-#         "shop_owner": "Potion Seller",
-#     }
+@router.get("/organization_info/")
+def get_organization_info():
+    """ """
+    sql = """
+    SELECT o.name
+    FROM organizations AS o
+    WHERE o.verified = TRUE
+    """
+    res = []
+    with db.engine.begin() as connection:
+        # Drop existing tables
+        rows = connection.execute(sqlalchemy.text(
+            sql))
+    for row in rows:
+        res.append({
+            "organization": row.name
+        })
+    return res
