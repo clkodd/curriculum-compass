e4a59e2f2b45ec988a009df4d133ac25f1d5a75f
clkodd <94199649+clkodd@users.noreply.github.com>
2023-11-06 20:53:28 -0800

    inserts

--- schema.sql
+++ schema.sql
@@ -60,3 +60,85 @@ create table
     constraint volunteers_email_key unique (email),
     constraint volunteers_volunteer_id_key unique (volunteer_id)
   ) tablespace pg_default;
+
+/* INSERTS
+-- Flow 1 Inserts: Volunteer Registering for an Event
+TRUNCATE TABLE organizations
+RESTART IDENTITY
+CASCADE;
+
+TRUNCATE TABLE volunteers
+RESTART IDENTITY
+CASCADE;
+
+INSERT INTO organizations (name, city, verified)
+VALUES ('Generic Helper Nonprofit', 'Los Angeles', TRUE);
+
+INSERT INTO supervisors (org_id, sup_name, email)
+VALUES (1, 'Michael Scott', 'mscott@dundermifflin.com');
+
+INSERT INTO events (sup_id, name, total_spots, min_age, activity_level, location, start_time, end_time, description) 
+VALUES (1, 'Soup kitchen', 30, 15, 1, 'Long Beach',  '2023-11-10T19:00:00+00:00', '2023-11-10T22:00:00+00:00', 'Hand out meals to the hungry.'), 
+(1, 'Beach cleanup', 15, 10, 2, 'Los Angeles', '2023-11-13T12:00:00+00:00', '2023-11-13T15:00:00+00:00', 'Come and collect trash from the beach!');
+
+----------
+
+-- Flow 2 Inserts: Volunteer Site Event Coordinator Event Posting
+TRUNCATE TABLE organizations
+RESTART IDENTITY
+CASCADE;
+
+TRUNCATE TABLE volunteers
+RESTART IDENTITY
+CASCADE;
+
+INSERT INTO organizations (name, city, verified)
+VALUES ('Midnight Mission', 'Los Angeles', TRUE);
+
+INSERT INTO supervisors (org_id, sup_name, email)
+VALUES (1, 'Ronnie Badonnie', 'ronnieb@midnightmish.com');
+
+----------
+
+-- Flow 3 Inserts: Volunteer Deleting an Event
+TRUNCATE TABLE organizations
+RESTART IDENTITY
+CASCADE;
+
+TRUNCATE TABLE volunteers
+RESTART IDENTITY
+CASCADE;
+
+INSERT INTO organizations (name, city, verified)
+VALUES ('Midnight Mission', 'Los Angeles', TRUE);
+
+INSERT INTO supervisors (org_id, sup_name, email)
+VALUES (1, 'Ronnie Badonnie', 'ronnieb@midnightmish.com');
+
+INSERT INTO events (sup_id, name, total_spots, min_age, activity_level, location, start_time, end_time, description) 
+VALUES (1, 'Midnight Mission Mania', 50, 15, 1, 'Los Angeles',  '2023-11-04T20:00:00Z', '2023-11-04T23:59:59Z', 'Serve food to the homeless at the Midnight Mission to help those in need.'); 
+
+----------
+
+-- Flow 4 Inserts: Admin Resetting the Site
+TRUNCATE TABLE organizations
+RESTART IDENTITY
+CASCADE;
+
+TRUNCATE TABLE volunteers
+RESTART IDENTITY
+CASCADE;
+
+INSERT INTO organizations (name, city, verified)
+VALUES ('Midnight Mission', 'Los Angeles', TRUE),
+('Generic Helper Nonprofit', 'Los Angeles', TRUE);
+
+INSERT INTO supervisors (org_id, sup_name, email)
+VALUES (1, 'Ronnie', 'ronnie@midnightmish.com'),
+(2, 'Michael Scott', 'mscott@dundermifflin.com');
+
+INSERT INTO events (sup_id, name, total_spots, min_age, activity_level, location, start_time, end_time, description) 
+VALUES (1, 'Soup kitchen', 30, 15, 1, 'Add me on Genshin Impact',  '2023-11-10T19:00:00+00:00', '2023-11-10T22:00:00+00:00', 'Hand out meals to the hungry.'), 
+(2, 'Beach cleanup', 15, 10, 2, 'Add me on Genshin Impact', '2023-11-13T12:00:00+00:00', '2020-11-13T15:00:00+00:00', 'Come and collect trash from the beach!');
+
+*/
